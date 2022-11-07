ee1d051478e6d3a8c311ad2776bddf2749e7aac0
Matt Wilson <mattwwilson34@gmail.com>
2022-11-07 12:29:04 -0500

    Add: random user db seeding

--- backend/src/database/database-seeding/seed.js
+++ backend/src/database/database-seeding/seed.js
@@ -2,6 +2,8 @@ import connectToDB from '../database-connection.js';
 import buildDB from './utils/build-db.js';
 import switchDB from './utils/switch-db.js';
 import buildDatabaseTables from './utils/build-db-tables.js';
+import { insertUser } from './utils/sql-queries.js';
+import randomUser from './utils/random-user.js';
 
 // DB connection
 const dbConnection = connectToDB();
@@ -12,5 +14,17 @@ buildDB(dbConnection);
 switchDB(dbConnection);
 
 buildDatabaseTables(dbConnection);
+
+// Add 10 random users to database
+try {
+  for (let i = 0; i < 10; i += 1) {
+    const user = randomUser();
+    dbConnection.query(insertUser(user), (err) => {
+      if (err) throw err;
+    });
+  }
+} catch (err) {
+  if (err) throw err;
+}
 
 dbConnection.end();

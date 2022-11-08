0cb2be310a35143016e1580eb2ff7258cedbe3a6
Matt Wilson <mattwwilson34@gmail.com>
2022-11-08 11:45:08 -0500

    Add: POST req functionality for backend login
    Setup basic login form that successfully hits server POST route.

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,20 +1,62 @@
 import './Login.css';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 
 const Login = ({ className }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // set fetch request options
+      const reqOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      };
+
+      // POST login data
+      const res = await fetch('http://localhost:8080/', reqOptions);
+
+      if (res.status === 200) {
+        setEmail('');
+        setPassword('');
+        setMessage('Login Successful.');
+      } else {
+        setMessage('Login failed.');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={className}>
-      <form>
-        <Input type='text' placeHolder='Email' />
-        <Input type='password' placeholder='Password' />
+      <form onSubmit={handleSubmit}>
+        <Input
+          type='text'
+          placeHolder='Email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type='password'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <Button submit text='Login' />
         <a href='https://www.google.com/'>Forgot Password</a>
         <hr />
         <Button submit={false} text='Create new account' />
       </form>
+      <div className='message'>{message ? <p>{message}</p> : null}</div>
     </div>
   );
 };
@@ -24,7 +66,7 @@ Login.propTypes = {
 };
 
 Login.defaultProps = {
-  className: 'text',
+  className: 'login',
 };
 
 export default Login;

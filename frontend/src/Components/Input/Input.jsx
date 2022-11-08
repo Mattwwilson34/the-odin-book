7580cd974ec6c294e2c1543a2347d409683ab30f
Matt Wilson <mattwwilson34@gmail.com>
2022-11-08 09:55:15 -0500

    Add: input component

---
+++ frontend/src/Components/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Input.css';
+
+const Input = ({ type, placeholder }) => {
+  return <input type={type} placeholder={placeholder} />;
+};
+
+Input.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  placeholder: 'Email',
+};
+
+export default Input;

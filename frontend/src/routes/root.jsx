0c35eb1d20076a34028ec5dbc157cf180a6055c0
Matt Wilson <mattwwilson34@gmail.com>
2022-11-10 09:03:17 -0500

    Add: module for checkIfLoggedIn func

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,29 +1,28 @@
-import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import checkIfLoggedIn from '../modules/checkIfLoggedIn';
 import Button from '../Components/Button/Button';
 
 const Root = () => {
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  (async () => {
-    const response = await axios
-      .get('http://localhost:8080/auth/user', { withCredentials: true })
-      .catch((err) => {
-        console.log('Not Authenticated prperly');
-        console.log(err);
-      });
+  // CHECK IF USER IS LOGGED IN
+  useEffect(() => {
+    (async () => {
+      const response = await checkIfLoggedIn();
+      if (response && response.data) {
+        console.log('User', response.data);
+        setUser(response.data.displayName);
+        setIsLoggedIn('true');
+      }
+    })();
+  }, []);
 
-    if (response && response.data) {
-      console.log('User', response.data);
-      setUser(response.data.displayName);
-      setIsLoggedIn('true');
-    }
-  })();
   return (
     <div>
       <h1>Welcome to The Odin Book</h1>
+      <a href='http://localhost:3000/login'>login</a>
 
       {user && <h1>{`Welcome ${user}`}</h1>}
 

f57f9cb7799eccd6ab82aa90593866c9efd4fe9f
Matt Wilson <mattwwilson34@gmail.com>
2022-11-10 11:52:34 -0500

    Add: mysql connection to passport google OAuth to handle new/old users
    When Google OAuth route is hit passport JS will check if user exists
    in sql db. If so user object is returned from db. If no user is found
    a new user will be created in the sql database.

--- backend/src/config/passport-setup.js
+++ backend/src/config/passport-setup.js
@@ -1,6 +1,10 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import * as dotenv from 'dotenv';
+import connectToDB from '../database/database-connection.js';
+import randomUser from '../database/database-seeding/utils/random-user.js';
+import { insertUser } from '../database/database-seeding/utils/sql-queries.js';
+import chalk from 'chalk';
 
 dotenv.config({
   path: '/Users/matthewwilson/Desktop/Coding/the-odin-project/the-odin-book/backend/.env',
@@ -15,6 +19,44 @@ passport.use(
       callbackURL: '/auth/google/redirect',
     },
     (request, accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      const db = connectToDB();
+      db.query(
+        `SELECT * FROM the_odin_book.users WHERE user_id = ${profile.id}`,
+        async (err, results) => {
+          if (err) throw new Error(err);
+
+          //! MYSQL QUERY EMPTY NO USER IN DB
+          if (!Array.isArray(results) || !results.length) {
+            //
+            console.log(chalk.red('No user in DB'));
+
+            // Create new user
+            let newUser = randomUser();
+            newUser = {
+              ...newUser,
+              id: profile._json.sub,
+              firstName: profile._json.given_name,
+              lastName: profile._json.family_name,
+              email: profile._json.email,
+            };
+
+            // Save new user to DB
+            db.query(insertUser(newUser), (err, results) => {
+              if (err) throw err;
+              console.log(results);
+            });
+          }
+
+          //! USER FOUND IN DB
+          else {
+            console.log(results);
+          }
+          // Close DB connection
+          await db.close();
+          console.log(chalk.yellow('DB connection closed'));
+        },
+      );
       done(null, profile);
     },
   ),

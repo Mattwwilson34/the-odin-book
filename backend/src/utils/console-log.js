929308aee2c71c8216579a3dc469602bc6c99422
Matt Wilson <mattwwilson34@gmail.com>
2022-11-10 18:39:56 -0500

    Add: Chalk / Console log util

---
+++ backend/src/utils/console-log.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+// Shorten logs for brevity
+const log = console.log;
+const danger = chalk.red;
+const warn = chalk.yellow;
+const success = chalk.green;
+
+export { log, danger, warn, success };

68a1b682e4120fdf9e02d8779d62d3a241c8fe50
Matt Wilson <mattwwilson34@gmail.com>
2022-11-10 19:09:59 -0500

    Refactor: convert db cb's to async/await

--- backend/src/config/passport-setup.js
+++ backend/src/config/passport-setup.js
@@ -1,14 +1,13 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import * as dotenv from 'dotenv';
-import connectToDB from '../database/database-connection.js';
+import dotenvConfig from './dontenv-config.js';
+import db from '../database/database-connection.js';
 import randomUser from '../database/database-seeding/utils/random-user.js';
 import { insertUser } from '../database/database-seeding/utils/sql-queries.js';
-import chalk from 'chalk';
+import { log, danger } from '../utils/console-log.js';
 
-dotenv.config({
-  path: '/Users/matthewwilson/Desktop/Coding/the-odin-project/the-odin-book/backend/.env',
-});
+dotenv.config(dotenvConfig);
 
 passport.use(
   new GoogleStrategy(
@@ -18,56 +17,57 @@ passport.use(
       clientSecret: process.env.GOOGLE_STRAT_SECRET,
       callbackURL: '/auth/google/redirect',
     },
-    (request, accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      const db = connectToDB();
-      db.query(
-        `SELECT * FROM the_odin_book.users WHERE user_id = ${profile.id}`,
-        async (err, results) => {
-          if (err) throw new Error(err);
+    async (request, accessToken, refreshToken, profile, done) => {
+      try {
+        const [rows, fields] = await db.query(
+          `SELECT * FROM the_odin_book.users WHERE user_id = ${profile.id}`,
+        );
 
-          //! MYSQL QUERY EMPTY NO USER IN DB
-          if (!Array.isArray(results) || !results.length) {
-            //
-            console.log(chalk.red('No user in DB'));
+        //! MYSQL QUERY EMPTY NO USER IN DB
+        if (!Array.isArray(rows) || !rows.length) {
+          //
+          log(danger('NO USER FOUND IN DB'));
 
-            // Create new user
-            let newUser = randomUser();
-            newUser = {
-              ...newUser,
-              id: profile._json.sub,
-              firstName: profile._json.given_name,
-              lastName: profile._json.family_name,
-              email: profile._json.email,
-            };
+          // Create new user
+          let newUser = randomUser();
+          newUser = {
+            ...newUser,
+            id: profile._json.sub,
+            firstName: profile._json.given_name,
+            lastName: profile._json.family_name,
+            email: profile._json.email,
+          };
 
-            // Save new user to DB
-            db.query(insertUser(newUser), (err, results) => {
-              if (err) throw err;
-              console.log(results);
-            });
+          // Save new user to DB
+          try {
+            const response = await db.query(insertUser(newUser));
+            log(response);
+          } catch (err) {
+            if (err) throw err;
           }
+        }
 
-          //! USER FOUND IN DB
-          else {
-            console.log(results);
-          }
-          // Close DB connection
-          await db.close();
-          console.log(chalk.yellow('DB connection closed'));
-        },
-      );
+        //! USER FOUND IN DB
+        else {
+          log(rows);
+        }
+      } catch (err) {
+        if (err) throw err;
+      }
       done(null, profile);
     },
   ),
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user.id);
 });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
+passport.deserializeUser(async (id, done) => {
+  const [rows] = await db.query(
+    `SELECT * FROM the_odin_book.users WHERE user_id = ${id}`,
+  );
+  done(null, rows[0]);
 });
 
 export default passport;

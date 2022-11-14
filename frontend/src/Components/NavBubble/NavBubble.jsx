6b11665058d69f886cb57fd9b121c4ca8f17431c
Matt Wilson <mattwwilson34@gmail.com>
2022-11-14 16:55:42 -0500

    Add: avatar identifier and styles

--- frontend/src/Components/NavBubble/NavBubble.jsx
+++ frontend/src/Components/NavBubble/NavBubble.jsx
@@ -2,15 +2,20 @@ import './NavBubble.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NavBubble = ({ icon, altText }) => (
+const NavBubble = ({ icon, altText, avatar }) => (
   <div className='NavBubble'>
-    <img src={icon} alt={altText} />
+    <img
+      className={avatar ? 'Nav_Bubble_Avatar' : 'Nav_Bubble_Icon'}
+      src={icon}
+      alt={altText}
+    />
   </div>
 );
 
 NavBubble.propTypes = {
   icon: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
+  avatar: PropTypes.bool.isRequired,
 };
 
 export default NavBubble;

cc8cbc50af144c00f0101a60007ef0ad69949870
Matt Wilson <mattwwilson34@gmail.com>
2022-11-14 18:18:14 -0500

    Add: Comment component with basic styles

---
+++ frontend/src/Components/Comment/__test__/Comment.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Comment from '../Comment';
+
+describe('Comment component', () => {
+  it('Does something...', () => {
+
+  });
+});

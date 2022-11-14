e11e59d7e22c70d85e14827d82092c223413569f
Matt Wilson <mattwwilson34@gmail.com>
2022-11-14 12:31:29 -0500

    Add: Navigation bar with log,search, nav links, and menu icons
    Using facebook navbar as comparison, I created a navigation bar that
    includes a logo, search bar, icon based navigation links, and four
    menu icons/bubbles. Functionaly for these components will be addressed
    following completion of dashboard design.

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import './Navbar.css';
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import NavLink from '../NavLink/NavLink';
+import NavBubble from '../NavBubble/NavBubble';
+import facebookIcon from '../../icons/facebook.svg';
+import homeIcon from '../../icons/home.svg';
+import menuIcon from '../../icons/menu-circles.svg';
+import messengerIcon from '../../icons/messenger.svg';
+import bellIcon from '../../icons/bell.svg';
+import avatarIcon from '../../icons/avatar.svg';
+
+const Navbar = () => (
+  <div className='Navbar'>
+    <div className='NavBar_Logo_Search_Container'>
+      <img src={facebookIcon} alt='facebook icon' />
+      <SearchBar />
+    </div>
+    <NavLink icon={homeIcon} activeURL='http://localhost:3000/' />
+    <div className='NavBubble_Container'>
+      <NavBubble icon={menuIcon} altText='Menu cirlces icon' />
+      <NavBubble icon={messengerIcon} altText='messenger icon' />
+      <NavBubble icon={bellIcon} altText='bell icon' />
+      <NavBubble icon={avatarIcon} altText='avatar icon' />
+    </div>
+  </div>
+);
+
+export default Navbar;

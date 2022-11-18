15ab5d7ff80ac28b3dc13c11a81de5dfa74677c5
Matt Wilson <mattwwilson34@gmail.com>
2022-11-18 16:02:05 -0500

    Add: sidebar elements and styles
    Added basic sidebar styles and elements. Link and icon functionality
    will come in later pull requests.

--- frontend/src/Components/RightSidebar/RightSidebar.jsx
+++ frontend/src/Components/RightSidebar/RightSidebar.jsx
@@ -1,6 +1,59 @@
 import './RightSidebar.css';
 import React from 'react';
 
-const RightSidebar = () => <h1 className='Right_Sidebar'>Right Sidebar</h1>;
+const RightSidebar = () => (
+  <div className='Right_Sidebar'>
+    <h3 className='Right_Sidebar_Header'>Contacts</h3>
+    <hr />
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-9.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-8.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-20.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-2.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-3.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+    <div className='Right_Sidebar_Contact_Container'>
+      <img
+        src='	http://localhost:8080/avatar-12.svg'
+        alt='avatar'
+        className='Right_Sidebar_Contact_Icon'
+      />
+      <span className='Right_Sidebar_Contact_Name'>Matt Wilson</span>
+    </div>
+  </div>
+);
 
 export default RightSidebar;

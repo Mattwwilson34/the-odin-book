cd88dc932d5defe520959a2dc8c82dd0375367a7
Matt Wilson <mattwwilson34@gmail.com>
2022-11-18 09:40:37 -0500

    Refactor: update SQL queries with new field names

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -11,20 +11,62 @@ router.get('/', (req, res) => {
 router.get('/posts', async (req, res) => {
   let combinedData = [];
 
-  const [posts] = await db.execute('SELECT * FROM user_post');
+  const postFields = [
+    'userPost.*',
+    'Users.firstName',
+    'Users.lastName',
+    'Users.profilePicture',
+  ];
+
+  const [posts] = await db.execute(
+    `SELECT ${postFields} FROM userPost INNER JOIN Users ON userPost.userID=Users.userID`,
+  );
 
   for await (const post of posts) {
     //
-    const userFields = ['profile_picture', 'first_name', 'last_name'];
-    const [postUserInfo] = await db.execute(
-      `SELECT ${userFields.join(',')} FROM users WHERE user_id = ?`,
-      [post.user_id],
+    let postData = [];
+    // Get post likes
+    const postLikeFields = ['createdDateTime', 'userID'];
+    const [postLikes] = await db.execute(
+      `SELECT ${postLikeFields} FROM postLike WHERE postID = ?`,
+      [post.postID],
     );
-    //
-    postUserInfo.forEach((userInfo) => {
-      const data = { ...post, userInfo };
-      combinedData.push(data);
-    });
+
+    let expandedPostLikeData = [];
+    // Get user names for likes
+    for await (const postLike of postLikes) {
+      const [postLikesFull] = await db.execute(
+        `SELECT firstName, lastName FROM Users WHERE userID = ?`,
+        [postLike.userID],
+      );
+      expandedPostLikeData.push(...postLikesFull);
+    }
+
+    let commentData = [];
+    // Get post comments
+    const [comments] = await db.execute(
+      `SELECT * FROM postComment WHERE postID = ?`,
+      [post.postID],
+    );
+
+    // Get user data amd comment likes for each comment
+    for await (const comment of comments) {
+      //
+      // Get user info
+      const [commentUserData] = await db.execute(
+        `SELECT firstName, lastName, profilePicture FROM Users WHERE userID = ?`,
+        [comment.userID],
+      );
+
+      // Get post comment likes
+      const [commentLikes] = await db.execute(
+        `SELECT * FROM commentLike WHERE postCommentID = ?`,
+        [comment.postCommentID],
+      );
+      commentData.push({ ...comment, commentUserData, commentLikes });
+    }
+    postData.push({ ...post, expandedPostLikeData, commentData });
+    combinedData.push(...postData);
   }
 
   res.json(combinedData);

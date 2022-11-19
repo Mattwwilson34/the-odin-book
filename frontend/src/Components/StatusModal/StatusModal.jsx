23d61c693f11fbbd2cb1c28cf1d80861cf84f26a
Matt Wilson <mattwwilson34@gmail.com>
2022-11-19 15:47:24 -0500

    Add: status modal styles and func to send post req to '/api/posts/'
    Added a modal that open when the status input button is clicked.
    Once open the user has the abillity to enter text into a textarea and
    then sumbit the post via an axio post request to the '/api/posts/ route.
    The user also has the option to click the X in the top right of the
    modal and this will close the modal.

---
+++ frontend/src/Components/StatusModal/StatusModal.jsx
@@ -0,0 +1,77 @@
+import './StatusModal.css';
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import NavBubble from '../NavBubble/NavBubble';
+
+const StatusModal = ({ user, setFetchPosts, setStatusModalOpen }) => {
+  //
+  // State
+  const [textArea, setTextArea] = useState('');
+
+  const handleTextAreaChange = (e) => setTextArea(e.target.value);
+
+  const handleStatusButtonClick = useCallback(() => {
+    setStatusModalOpen((prev) => !prev);
+  }, [setStatusModalOpen]);
+
+  const submitPost = async () => {
+    const postURL = 'http://localhost:8080/api/posts';
+
+    const post = {
+      userID: user.userID,
+      postBody: textArea,
+    };
+
+    try {
+      const response = await axios.post(postURL, post);
+      handleStatusButtonClick();
+      setFetchPosts(true);
+      console.log(response.data);
+    } catch (err) {
+      if (err) throw err;
+    }
+  };
+
+  return (
+    <div className='Status_Modal'>
+      <div className='Status_Modal_Form'>
+        <div className='Status_Modal_Form_Header_Container'>
+          <span className='Status_Modal_Form_Header'>Create Post</span>
+          <button
+            type='button'
+            className='Status_Modal_Form_Exit_Btn'
+            onClick={handleStatusButtonClick}
+          >
+            &#x2715;
+          </button>
+        </div>
+        <hr />
+        <div className='Status_Modal_User_Info_Container'>
+          <NavBubble avatar icon={user.profilePicture} />
+          <span>{`${user.firstName} ${user.lastName}`}</span>
+        </div>
+        <textarea
+          className='Status_Modal_Form_Textarea'
+          onChange={handleTextAreaChange}
+          id='story'
+          name='story'
+          rows='5'
+          cols='33'
+          placeholder={`What's on your mind ${user.firstName}`}
+        />
+        <button type='button' onClick={submitPost}>
+          Post
+        </button>
+      </div>
+    </div>
+  );
+};
+
+StatusModal.propTypes = {
+  user: PropTypes.object.isRequired,
+  setFetchPosts: PropTypes.func.isRequired,
+  setStatusModalOpen: PropTypes.func.isRequired,
+};
+
+export default StatusModal;

b714a5c3bc6362051150a59ee14e8222c1723130
Matt Wilson <mattwwilson34@gmail.com>
2022-11-20 11:27:57 -0500

    Add: expand input on click of post comment button + input styles

--- frontend/src/Components/CommentInput/CommentInput.jsx
+++ frontend/src/Components/CommentInput/CommentInput.jsx
@@ -1,6 +1,22 @@
-import './CommentInput.css'
+import './CommentInput.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CommentInput = () => <h1>CommentInput COMPONENT</h1>;
+import NavBubble from '../NavBubble/NavBubble';
+import Input from '../Input/Input';
 
-  export default CommentInput;
+const CommentInput = ({ user }) => (
+  <div className='Comment_Input'>
+    <div className='Comment_Input_Container'>
+      <NavBubble avatar icon={user.profilePicture} altText='avatar' />
+      <Input type='text' placeholder='Write a comment...' />
+    </div>
+    <span className='Comment_Input_Tooltip'>Press Enter to post.</span>
+  </div>
+);
+
+CommentInput.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default CommentInput;

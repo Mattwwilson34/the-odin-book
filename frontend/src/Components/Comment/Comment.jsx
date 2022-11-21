6f064e06fb817c4b744f2ce5c1e2599fa155593b
Matt Wilson <mattwwilson34@gmail.com>
2022-11-21 18:03:05 -0500

    Add: comment like db updating via axios post to /api/commentLike

--- frontend/src/Components/Comment/Comment.jsx
+++ frontend/src/Components/Comment/Comment.jsx
@@ -1,14 +1,58 @@
 import './Comment.css';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import NavBubble from '../NavBubble/NavBubble';
 
-const Comment = ({ commentData }) => {
-  const { commentUserData, commentLikes, commentText, createdDateTime } =
-    commentData;
+const Comment = ({ commentData, userID, setFetchPosts }) => {
+  //
+  // State
+  const [isCommentLiked, setIsCommentLiked] = useState(false);
+  const [postCommentData, setPostCommentData] = useState(false);
+  const isMounted = useRef(false);
+
+  const {
+    commentUserData,
+    postCommentID,
+    commentLikes,
+    commentText,
+    createdDateTime,
+  } = commentData;
 
   const { firstName, lastName, profilePicture } = commentUserData[0];
+
+  const handleLikeClick = () => {
+    setIsCommentLiked((prev) => !prev);
+    setPostCommentData(true);
+  };
+
+  const submitCommentLike = async () => {
+    const commentLikeURL = 'http://localhost:8080/api/commentLike';
+
+    const commentLikeData = {
+      postCommentID,
+      userID,
+      liked: isCommentLiked,
+    };
+
+    try {
+      const response = await axios.post(commentLikeURL, commentLikeData);
+      setFetchPosts(true);
+      console.log(response.data.message);
+    } catch (err) {
+      if (err) throw err;
+    }
+  };
+
+  useEffect(() => {
+    if (isMounted.current && postCommentData) {
+      submitCommentLike();
+      setPostCommentData(false);
+    } else {
+      isMounted.current = true;
+    }
+  }, [isCommentLiked]);
 
   return (
     <div className='Comment_Container'>
@@ -38,10 +82,13 @@ const Comment = ({ commentData }) => {
 Comment.propTypes = {
   commentData: PropTypes.shape({
     commentUserData: PropTypes.array.isRequired,
+    postCommentID: PropTypes.string.isRequired,
     commentLikes: PropTypes.array.isRequired,
     commentText: PropTypes.string.isRequired,
     createdDateTime: PropTypes.string.isRequired,
   }).isRequired,
+  userID: PropTypes.string.isRequired,
+  setFetchPosts: PropTypes.func.isRequired,
 };
 
 export default Comment;

a745a68a7eaee3e4004807c791bddb4b5d742b6d
Matt Wilson <mattwwilson34@gmail.com>
2022-11-21 12:35:21 -0500

    Add: onKeyPress, onChange, className props

--- frontend/src/Components/Input/Input.jsx
+++ frontend/src/Components/Input/Input.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Input.css';
 
-const Input = ({ type, placeholder }) => (
-  <input type={type} placeholder={placeholder} />
+const Input = ({ type, placeholder, onKeyPress, onChange, className }) => (
+  <input
+    type={type}
+    placeholder={placeholder}
+    className={className}
+    onChange={onChange}
+    onKeyPress={onKeyPress}
+  />
 );
 
 Input.propTypes = {
   type: PropTypes.string,
   placeholder: PropTypes.string,
+  onKeyPress: PropTypes.func,
+  onChange: PropTypes.func,
+  className: PropTypes.string,
 };
 
 Input.defaultProps = {
   type: 'text',
   placeholder: 'Email',
+  className: '',
+  onKeyPress: () => false,
+  onChange: () => false,
 };
 
 export default Input;

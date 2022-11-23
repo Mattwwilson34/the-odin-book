ae4e441593406fea2284b9491f6c0efb6954a66a
Matt Wilson <mattwwilson34@gmail.com>
2022-11-23 09:55:38 -0500

    Add: clickHandler to redirect to /profile

--- frontend/src/Components/NavBubble/NavBubble.jsx
+++ frontend/src/Components/NavBubble/NavBubble.jsx
@@ -1,22 +1,37 @@
 import './NavBubble.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const NavBubble = ({ icon, altText, avatar }) => (
-  <div className='NavBubble'>
-    <img
-      className={avatar ? 'Nav_Bubble_Avatar' : 'Nav_Bubble_Icon'}
-      src={icon}
-      alt={altText}
-      referrerPolicy='no-referrer'
-    />
-  </div>
-);
+const NavBubble = ({ icon, altText, avatar, openProfile }) => {
+  //
+  const navigate = useNavigate();
+
+  const redirectToProfile = () => (openProfile ? navigate('/profile') : false);
+
+  return (
+    <div
+      className='NavBubble'
+      onClick={redirectToProfile}
+      onKeyPress={() => false}
+      role='button'
+      tabIndex='0'
+    >
+      <img
+        className={avatar ? 'Nav_Bubble_Avatar' : 'Nav_Bubble_Icon'}
+        src={icon}
+        alt={altText}
+        referrerPolicy='no-referrer'
+      />
+    </div>
+  );
+};
 
 NavBubble.propTypes = {
   icon: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
   avatar: PropTypes.bool.isRequired,
+  openProfile: PropTypes.bool.isRequired,
 };
 
 export default NavBubble;

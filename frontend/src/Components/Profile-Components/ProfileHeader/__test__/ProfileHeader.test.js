1a6de5caeddd28884a7eb7e054754c5987b9b08e
Matt Wilson <mattwwilson34@gmail.com>
2022-11-26 13:16:01 -0500

    initial commit

---
+++ frontend/src/Components/Profile-Components/ProfileHeader/__test__/ProfileHeader.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProfileHeader from '../ProfileHeader';
+
+describe('ProfileHeader component', () => {
+  it('Does something...', () => {
+
+  });
+});

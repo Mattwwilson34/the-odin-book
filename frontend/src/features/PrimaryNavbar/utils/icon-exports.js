8a6a9c34928b526ee153a5c67ce33a8f2f5ec7bf
Matt Wilson <mattwwilson34@gmail.com>
2022-11-30 16:46:14 -0500

    Refactor: add icon export file for cleaner imports

---
+++ frontend/src/features/PrimaryNavbar/utils/icon-exports.js
@@ -0,0 +1,7 @@
+import bell from '../assets/icons/bell.svg';
+import home from '../assets/icons/home.svg';
+import circle from '../assets/icons/menu-circles.svg';
+import messenger from '../assets/icons/messenger.svg';
+import magnifyingGlass from '../assets/icons/search.svg';
+
+export { bell, home, circle, messenger, magnifyingGlass };

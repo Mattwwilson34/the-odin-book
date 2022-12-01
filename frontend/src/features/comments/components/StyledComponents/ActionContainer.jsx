70dbe3c1d19dd66aecd212ab40da4c70caed4f8c
Matt Wilson <mattwwilson34@gmail.com>
2022-12-01 17:15:32 -0500

    Add: styled components for better ogranization

---
+++ frontend/src/features/comments/components/StyledComponents/ActionContainer.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import ContentContainer from '../../../../components/StyledComponents/ContentContainer';
+
+const ActionContainer = styled(ContentContainer)`
+  width: 100%;
+  background-color: white;
+  gap: 10px;
+  padding: 5px 0 0 10px;
+  margin-left: 40px;
+`;
+
+export default ActionContainer;

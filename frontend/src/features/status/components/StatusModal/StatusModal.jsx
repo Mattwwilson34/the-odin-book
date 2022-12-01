d8bd550bfd4b1ae06d42932afce65b1f33c13358
Matt Wilson <mattwwilson34@gmail.com>
2022-12-01 14:22:53 -0500

    Refactor: to styled components for dryer code

---
+++ frontend/src/features/status/components/StatusModal/StatusModal.jsx
@@ -0,0 +1,52 @@
+import { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '../../../../components/Avatar';
+
+import ContentContainer from '../../../../components/StyledComponents/ContentContainer';
+import Modal from '../../../../components/StyledComponents/Modal';
+import ExitButton from '../../../../components/StyledComponents/ExitButton';
+import TextArea from '../../../../components/StyledComponents/TextArea';
+import Button from '../../../../components/StyledComponents/Button';
+import Span from '../../../../components/StyledComponents/Span';
+
+import submitStatus from '../../api/status-api';
+
+const StatusModal = ({ user, setModalOpen }) => {
+  const [textArea, setTextArea] = useState(null);
+
+  const handleChange = (e) => setTextArea(e.target.value);
+
+  const handleClick = useCallback(() => {
+    setModalOpen((prev) => !prev);
+  }, [setModalOpen]);
+
+  const submitPost = async () => {
+    submitStatus(user.userID, textArea);
+    setModalOpen(false);
+  };
+
+  return (
+    <Modal>
+      <ContentContainer width='400px' height='400px'>
+        <Span fontSize='2rem' margin='auto' left='45px' bold>
+          Create Post
+        </Span>
+        <ExitButton onClick={handleClick} />
+        <hr />
+        <Avatar user={user} />
+        <Span margin='0 10px' bold>{`${user.firstName} ${user.lastName}`}</Span>
+        <TextArea user={user} onChange={handleChange} />
+        <Button disabled={!textArea} onClick={submitPost}>
+          Post
+        </Button>
+      </ContentContainer>
+    </Modal>
+  );
+};
+
+StatusModal.propTypes = {
+  user: PropTypes.object.isRequired,
+  setModalOpen: PropTypes.func.isRequired,
+};
+
+export default StatusModal;

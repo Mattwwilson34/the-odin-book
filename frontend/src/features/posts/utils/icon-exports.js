30164262f8d7c8a81113962018566e91ee8f7695
Matt Wilson <mattwwilson34@gmail.com>
2022-12-02 11:20:23 -0500

    Add: icons exports to reduce import clutter

---
+++ frontend/src/features/posts/utils/icon-exports.js
@@ -0,0 +1,5 @@
+import like from '../assets/icons/comment.svg';
+import comment from '../assets/icons/forward.svg';
+import forward from '../assets/icons/like.svg';
+
+export { like, comment, forward };

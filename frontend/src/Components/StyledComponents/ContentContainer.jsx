dba355922c7ff9d0bfb4aac6c11ca8c81ad84eda
Matt Wilson <mattwwilson34@gmail.com>
2022-12-03 13:39:08 -0500

    Add: prop based style options

--- frontend/src/Components/StyledComponents/ContentContainer.jsx
+++ frontend/src/Components/StyledComponents/ContentContainer.jsx
@@ -4,9 +4,11 @@ const ContentContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  padding: 10px;
+  padding: ${({ padding }) => padding || '10px'};
   background: white;
   border-radius: 10px;
+  font-size: ${({ fontSize }) => fontSize};
+  margin: ${({ margin }) => margin};
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   border: ${(props) => (props.border ? '2px dashed red' : 'none')};

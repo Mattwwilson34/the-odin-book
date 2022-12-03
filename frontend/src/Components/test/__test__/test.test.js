129964e80f79d35b6ce35be5156e558afafb8741
Matt Wilson <mattwwilson34@gmail.com>
2022-12-03 14:16:23 -0500

    Cut

--- frontend/src/Components/test/__test__/test.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import test from '../test';
-
-describe('test component', () => {
-  it('Does something...', () => {
-
-  });
-});
--- frontend/src/Components/test/test.jsx
+++
@@ -1,6 +0,0 @@
-import './test.css';
-import React from 'react';
-
-export const test = () => <h1>test lal</h1>;
-
-export default test;

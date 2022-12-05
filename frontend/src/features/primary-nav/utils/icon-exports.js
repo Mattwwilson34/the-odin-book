d24c299c3379ede5be64d6f33428680c0f9690d3
Matt Wilson <mattwwilson34@gmail.com>
2022-12-05 05:43:54 -0500

    Add: facebook svg to exports

--- frontend/src/features/primary-nav/utils/icon-exports.js
+++ frontend/src/features/primary-nav/utils/icon-exports.js
@@ -3,5 +3,6 @@ import home from '../assets/icons/home.svg';
 import circle from '../assets/icons/menu-circles.svg';
 import messenger from '../assets/icons/messenger.svg';
 import magnifyingGlass from '../assets/icons/search.svg';
+import facebook from '../assets/icons/facebook.svg';
 
-export { bell, home, circle, messenger, magnifyingGlass };
+export { bell, home, circle, messenger, magnifyingGlass, facebook };

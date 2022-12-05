2fc25b5e489385d34802c6022108d92509204a56
Matt Wilson <mattwwilson34@gmail.com>
2022-12-04 19:25:13 -0500

    Cut: no tests currently

--- frontend/src/Components/Timeline/__test__/Timeline.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Timeline from '../Timeline';
-
-describe('Timeline component', () => {
-  it('Does something...', () => {
-
-  });
-});

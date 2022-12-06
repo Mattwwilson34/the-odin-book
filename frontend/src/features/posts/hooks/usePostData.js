26a5b87c4ecb2d6806dfb3e158e43664e0a9ae5e
Matt Wilson <mattwwilson34@gmail.com>
2022-12-06 17:13:55 -0500

    Cut: remove on success/error funcs

--- frontend/src/features/posts/hooks/usePostData.js
+++ frontend/src/features/posts/hooks/usePostData.js
@@ -1,13 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import getPosts from '../api/getPosts';
 
-const onSuccess = () => console.log('Successfuylly fetched posts');
-const onError = () => console.log('Failed to fetch posts');
-
-const usePostData = () =>
-  useQuery(['posts'], getPosts, {
-    onSuccess,
-    onError,
-  });
+const usePostData = () => useQuery(['posts'], getPosts);
 
 export default usePostData;

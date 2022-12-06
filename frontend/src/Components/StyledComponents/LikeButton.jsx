91f6163ef7a1bf9902cb270455414a1c8fbb862a
Matt Wilson <mattwwilson34@gmail.com>
2022-12-06 17:12:03 -0500

    Fix: like prop boolean issue via transient props

--- frontend/src/Components/StyledComponents/LikeButton.jsx
+++ frontend/src/Components/StyledComponents/LikeButton.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import SVG from 'react-inlinesvg';
 import styled from 'styled-components';
 
@@ -20,6 +21,7 @@ const StyledButton = styled.button`
   }
 
   &[disabled] {
+    pointer-events: none;
     color: black;
   }
 `;
@@ -31,28 +33,33 @@ const StyledSVG = styled(SVG).attrs(() => ({
   padding: 0;
   width: ${(props) => props.width || '24px'};
   border-radius: 50%;
-  background-color: ${(props) => (props.liked ? '#1a76f2' : 'transparent')};
+  background-color: ${(props) => (props.$liked ? '#1a76f2' : 'transparent')};
 
   & path {
-    fill: ${({ liked }) => (liked ? 'white' : 'transparent')};
-    stroke: ${({ liked }) => (liked ? '#1a76f2' : 'black')};
+    fill: ${(props) => (props.$liked ? 'white' : 'transparent')};
+    stroke: ${(props) => (props.$liked ? '#1a76f2' : 'black')};
   }
 `;
 
-// eslint-disable-next-line react/prop-types
 const LikeButton = ({ text, disabled }) => {
   const [liked, setLiked] = useState(false);
 
   return (
-    <StyledButton
-      liked={liked}
-      disabled={disabled}
-      onClick={() => setLiked((prev) => !prev)}
-    >
-      <StyledSVG liked={liked} />
+    <StyledButton disabled={disabled} onClick={() => setLiked((prev) => !prev)}>
+      <StyledSVG $liked={liked} />
       <Span fontSize='16px'>{text}</Span>
     </StyledButton>
   );
+};
+
+LikeButton.propTypes = {
+  text: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+LikeButton.defaultProps = {
+  text: '',
+  disabled: false,
 };
 
 export default LikeButton;

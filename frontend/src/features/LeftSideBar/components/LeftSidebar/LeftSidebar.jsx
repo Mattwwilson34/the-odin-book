6ad9b2005758199e38f9ca801a84a9b43d2c7505
Matt Wilson <mattwwilson34@gmail.com>
2022-12-07 13:08:13 -0500

    Refactor: to styled components

---
+++ frontend/src/features/LeftSideBar/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as icons from '../../utils/icon-exports';
+
+import Avatar from '../../../../components/Avatar';
+import IconButton from '../../../../components/StyledComponents/IconButton';
+import ContentContainer from '../../../../components/StyledComponents/ContentContainer';
+import Span from '../../../../components/StyledComponents/Span';
+import SidebarContainer from '../StyledComponents/SidebarContainer';
+import HoverContainer from '../../../../components/StyledComponents/hoverContainer';
+
+const LeftSidebar = ({ user }) => (
+  <SidebarContainer>
+    <HoverContainer>
+      <ContentContainer padding='0' background='transparent' gap='10px'>
+        <Avatar user={user} />
+        <Span bold>{`${user.firstName} ${user.lastName}`}</Span>
+      </ContentContainer>
+    </HoverContainer>
+    <HoverContainer>
+      <IconButton
+        svg={icons.friends}
+        text='friends'
+        width='40px'
+        padding='0'
+        margin='0 0 0 10px'
+        bold
+      />
+    </HoverContainer>
+    <HoverContainer>
+      <IconButton
+        svg={icons.calendar}
+        text='events'
+        width='40px'
+        padding='0'
+        margin='0 0 0 10px'
+        bold
+      />
+    </HoverContainer>
+    <HoverContainer>
+      <IconButton
+        svg={icons.groups}
+        text='groups'
+        width='40px'
+        padding='0'
+        margin='0 0 0 10px'
+        bold
+      />
+    </HoverContainer>
+  </SidebarContainer>
+);
+
+LeftSidebar.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default LeftSidebar;

cedc8f041a9a9d2df12bda05b5743bb744276617
Matt Wilson <mattwwilson34@gmail.com>
2022-12-07 13:10:10 -0500

    Cut: unneeded components

--- frontend/src/routes/profile.jsx
+++ frontend/src/routes/profile.jsx
@@ -1,15 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import Navbar from '../features/primary-nav/components/Navbar';
-import ProfileHeader from '../components/Profile-Components/ProfileHeader/ProfileHeader';
-import ProfileBody from '../components/Profile-Components/PofileBody/PofileBody';
 
 const Profile = () => {
   const { state: user } = useLocation();
   return (
     <div>
       <Navbar user={user} />
-      <ProfileHeader user={user} />
-      <ProfileBody />
     </div>
   );
 };

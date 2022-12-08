c324cf0dcf1ea1adf0dd0da1cc8299b2cef54ab0
Matt Wilson <mattwwilson34@gmail.com>
2022-12-08 14:29:35 -0500

    Add: Profile sub component

---
+++ frontend/src/features/profile/components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import IntroContainer from '../StyledComponents/IntroContainer';
+import IntroButton from '../StyledComponents/IntroButton';
+
+const Intro = () => (
+  <IntroContainer>
+    <h2>Intro</h2>
+    <IntroButton>Add bio</IntroButton>
+    <IntroButton>Edit details</IntroButton>
+    <IntroButton>Add hobbies</IntroButton>
+  </IntroContainer>
+);
+
+export default Intro;

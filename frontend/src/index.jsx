96886d905f0b9f6c7bd2f79275cc19536ce622f2
Matt Wilson <mattwwilson34@gmail.com>
2022-12-08 14:12:10 -0500

    Add: Error component to progile page

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './routes/home.page';
-import Profile from './routes/profile';
+import Profile from './routes/profile.page';
 import LoginSuccess from './routes/login-success';
 import ErrorPage from './utils/error-page';
 import './index.css';
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
   {
     path: '/profile',
     element: <Profile />,
+    errorElement: <ErrorPage />,
   },
 ]);
 

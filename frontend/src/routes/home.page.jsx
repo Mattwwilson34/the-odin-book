437581284be4a60fc88be18afb9c949cbda80de7
Matt Wilson <mattwwilson34@gmail.com>
2022-12-08 14:30:48 -0500

    Add: leftsidebar feature to home.page route

--- frontend/src/routes/home.page.jsx
+++ frontend/src/routes/home.page.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Login from '../features/login/components/Login';
 import Navbar from '../features/primary-nav';
-import LeftSidebar from '../features/LeftSideBar';
+import LeftSidebar from '../features/leftsidebar';
 import Timeline from '../features/timeline/components/Timeline/Timeline';
 
 import useUserData from '../hooks/useUerData';

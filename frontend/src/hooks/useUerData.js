aa5c0bffa94b03ed31140c9eda1e0850b68ad7aa
Matt Wilson <mattwwilson34@gmail.com>
2022-12-09 18:03:13 -0500

    Fix: filename

--- frontend/src/hooks/useUerData.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import getUser from '../utils/getUser';
-
-const useUserData = () =>
-  useQuery(['user'], getUser, {
-    retry: false,
-  });
-
-export default useUserData;

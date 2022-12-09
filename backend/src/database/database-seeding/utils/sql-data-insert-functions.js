02401eff4c031ebb7f10d3f1a5b23925b0ef5cba
Matt Wilson <mattwwilson34@gmail.com>
2022-12-08 21:19:01 -0500

    Add: friendship db seed function
    This function adds a random number of friendships to each user in the
    db. It also makes sure that repeate friendships do not occur and the a
    user is not friended with themsleves.
    
    Due to the looped nature of these checks and corrections that needed to
    be made to ensure the above I needed to store all the correct friendship
    data into one large array and then batch insert this via the mysql2
    library.

--- backend/src/database/database-seeding/utils/sql-data-insert-functions.js
+++ backend/src/database/database-seeding/utils/sql-data-insert-functions.js
@@ -114,10 +114,65 @@ const insertRandomPostLikeToDB = async (numberOfPostLikes = 300) => {
   );
 };
 
+const insertRandomFriendshipsToDB = async (friendsPerUser = 5) => {
+  //
+  //batch insert array
+  const friendshipDataArrayContainer = [];
+
+  // Get userIds from db
+  const [userIds] = await db.query('SELECT userID from Users');
+
+  userIds.forEach(async (userId, index) => {
+    // destructure userID so that comparisons are [string <> string] not [string <> obj]
+    const { userID: userIdOne } = userId;
+
+    // holds existing friendships to prevent duplicate entries
+    let alreadyFriends = [];
+
+    // create random friendships *no repeate friendships*
+    for (let i = 0; i < friendsPerUser; i += 1) {
+      let userIdTwo = userIds[getRandomNumber(0, userIds.length - 1)].userID;
+
+      // get new userIdTwo if same as userId or if friendship already exists
+      while (userIdTwo === userIdOne || alreadyFriends.includes(userIdTwo)) {
+        userIdTwo = userIds[getRandomNumber(0, userIds.length - 1)].userID;
+      }
+
+      // userIdTwo passed validation add to alreadyFriends array to prevent duplicate
+      alreadyFriends.push(userIdTwo);
+
+      // get friendship object with all needed fields
+      const friendShip = fakeData.randomFriendship(userIdOne, userIdTwo);
+
+      // get array of friendship data fieldso
+      const friendShipDataArray = sqlQuery.insertFriendship(friendShip);
+
+      // add
+      friendshipDataArrayContainer.push(friendShipDataArray);
+    }
+    // reset friends array for next user iteration
+    alreadyFriends.length = 0;
+  });
+
+  // batch insert all friendships into friendship table
+  const sql = `INSERT INTO friends (userIdOne,userIdTwo,friendshipStatus,createdDateTime)VALUES ?`;
+  try {
+    await db.query(sql, [friendshipDataArrayContainer], true);
+  } catch (err) {
+    if (err) throw err;
+  }
+
+  const totalFriends = userIds.length * friendsPerUser;
+  log(success(`✅ Added ${totalFriends} friendships to Friends Table`));
+};
+
+// insertRandomFriendshipsToDB();
+
 export {
   insertRandomUserToDB,
   insertRandomPostToDB,
   insertRandomCommentToDB,
   insertRandomCommentLikeToDB,
   insertRandomPostLikeToDB,
+  insertRandomFriendshipsToDB,
 };

a3420ce0aaf995fc3afa2ddd54f7dc6b70cd289a
Matt Wilson <mattwwilson34@gmail.com>
2022-12-16 18:20:28 -0500

    Add: styled components to be used with Friends and FriendCard components

---
+++ frontend/src/features/friends/components/StyledComponents/CardContentContainer.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import ContentContainer from '../../../../components/StyledComponents/ContentContainer';
+
+const CardContentContainer = styled(ContentContainer)`
+  padding: 0;
+  align-items: flex-start;
+  gap: 10px;
+  flex: 0 1 200px;
+  justify-content: center;
+`;
+
+export default CardContentContainer;

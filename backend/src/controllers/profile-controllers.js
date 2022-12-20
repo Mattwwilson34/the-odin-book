8343d139867553dc0f7ed40d04a5fcab5e96fe8b
Matt Wilson <mattwwilson34@gmail.com>
2022-12-20 18:51:54 -0500

    Add: friend data population to profile route

--- backend/src/controllers/profile-controllers.js
+++ backend/src/controllers/profile-controllers.js
@@ -11,9 +11,24 @@ const getProfile = async (req, res) => {
 
   const [userPhotos] = await db.execute(photoSql, [userID]);
 
+  const friendSql = `SELECT userIdTwo FROM Friends WHERE userIdOne = ? AND friendshipStatus = ?`;
+
+  const [userFriends] = await db.execute(friendSql, [userID, '1']);
+
+  const userFriendIDArray = [];
+
+  userFriends.forEach((friendID) =>
+    userFriendIDArray.push(`"${friendID.userIdTwo}"`),
+  );
+
+  const [friendData] = await db.execute(
+    `SELECT userID, firstName, lastName, username, profilePicture FROM the_odin_book.Users WHERE userID IN (${userFriendIDArray})`,
+  );
+
   const combinedData = {
     ...user[0],
     userPhotos,
+    friendData,
   };
 
   res.send(combinedData);

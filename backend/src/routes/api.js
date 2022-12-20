84a9603588489fbdf085ec9c4d0de6e381d1c061
Matt Wilson <mattwwilson34@gmail.com>
2022-12-20 18:43:26 -0500

    Cut: unneeded routes

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,11 +4,9 @@ import * as posts from '../controllers/post-controllers.js';
 import * as comments from '../controllers/comment-controllers.js';
 import * as friends from '../controllers/friend-controllers.js';
 import getProfile from '../controllers/profile-controllers.js';
-import getPhotos from '../controllers/photo-controller.js';
 
 const router = express.Router();
 
-router.get('/', (res) => res.send('<h1>You hit the API route</h1>'));
 
 router.post('/signup', postSignupController);
 
@@ -31,8 +29,5 @@ router.put('/friends/update-friendship', friends.updateFriendsController);
 
 // PROFILES
 router.get('/profile/:userID', getProfile);
-
-// PHOTOS
-router.get('/photos/:userID', getPhotos);
 
 export default router;

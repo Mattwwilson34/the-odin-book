b40508271c30b3fd2e592fa48c219e367f980164
Matt Wilson <mattwwilson34@gmail.com>
2022-12-20 13:32:15 -0500

    Add: dynamic photo generation and styles on user profile pages

--- frontend/src/features/profile/components/Intro/Intro.jsx
+++ frontend/src/features/profile/components/Intro/Intro.jsx
@@ -1,13 +1,31 @@
+import PropTypes from 'prop-types';
 import IntroContainer from '../StyledComponents/IntroContainer';
 import IntroButton from '../StyledComponents/IntroButton';
 
-const Intro = () => (
-  <IntroContainer>
-    <h2>Intro</h2>
-    <IntroButton>Add bio</IntroButton>
-    <IntroButton>Edit details</IntroButton>
-    <IntroButton>Add hobbies</IntroButton>
-  </IntroContainer>
-);
+const Intro = ({ admin, profile }) =>
+  admin ? (
+    <IntroContainer>
+      <h2>Intro</h2>
+      <IntroButton>Add bio</IntroButton>
+      <IntroButton>Edit details</IntroButton>
+      <IntroButton>Add hobbies</IntroButton>
+    </IntroContainer>
+  ) : (
+    <IntroContainer>
+      <h2>Intro</h2>
+      <p>{profile.bio}</p>
+      <p>{profile.jobTitle}</p>
+      <p>{`${profile.city}, ${profile.state}`}</p>
+    </IntroContainer>
+  );
+
+Intro.propTypes = {
+  admin: PropTypes.bool,
+  profile: PropTypes.object.isRequired,
+};
+
+Intro.defaultProps = {
+  admin: false,
+};
 
 export default Intro;
--- frontend/src/features/profile/components/Photos/Photos.jsx
+++ frontend/src/features/profile/components/Photos/Photos.jsx
@@ -1,19 +1,45 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import PhotoContainer from '../StyledComponents/PhotoContainer';
 import Image from '../StyledComponents/Image';
-import seedImage from '../../assets/images/seedImage.jpg';
 
-const Photos = () => (
-  <PhotoContainer>
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-    <Image src={seedImage} />
-  </PhotoContainer>
-);
+const Header = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 1.5rem;
+  font-family: 'klavika-medium';
+
+  & > a {
+    font-size: 1.25rem;
+  }
+
+  & > div {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const Photos = ({ profile }) => {
+  const { userPhotos } = profile;
+  const reducedPhotos = userPhotos.slice(0, 9);
+
+  return (
+    <PhotoContainer>
+      <Header>
+        Photos
+        <Link to='/'>See all photos</Link>
+      </Header>
+      {reducedPhotos.map((photo) => (
+        <Image src={photo.photoURL} key={photo.photoID} />
+      ))}
+    </PhotoContainer>
+  );
+};
+Photos.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default Photos;

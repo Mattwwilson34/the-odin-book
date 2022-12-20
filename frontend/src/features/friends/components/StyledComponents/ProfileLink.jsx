c9a8d1515ead197241b711eb22959917b6557ade
Matt Wilson <mattwwilson34@gmail.com>
2022-12-20 14:03:26 -0500

    Add: React styled link to wrap friend avatars

---
+++ frontend/src/features/friends/components/StyledComponents/ProfileLink.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ProfileLink = styled(Link)`
+  width: 100%;
+  color: black;
+  text-align: center;
+
+  &:hover {
+    color: black;
+  }
+`;
+export default ProfileLink;
